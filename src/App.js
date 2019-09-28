5637dbd636797d2ec9913f3b89b81df2ea604c67
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 19:01:11 -0700

    import connect in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <p>Hello World</p>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
