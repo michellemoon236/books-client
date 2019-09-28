5de5b511b203b2f9b8676f1351709a01dc5e1cd8
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:22:11 -0700

    create Navigation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import GenresContainer from './containers/GenresContainer';
 import BooksContainer from './containers/BooksContainer';
 
@@ -9,7 +11,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
+        <Router>
+          <Navigation />
+        </Router>
         <GenresContainer />
         <BooksContainer />
       </div>
