494ca014f04f7ddb2ddbf31304aa1239042d3425
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:14:16 -0700

    add title to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      
+      <br />
+      <h1>Welcome to Book Tally</h1>
+      <h3>keep track of the books you want to read</h3>
     </div>
   )
 }
