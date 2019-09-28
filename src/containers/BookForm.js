00d717a89941458609f94a3f377f93490d1e2b83
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:17:03 -0700

    import connect to Book Form

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class BookForm extends Component {
   render() {
@@ -10,4 +11,4 @@ class BookForm extends Component {
   }
 }
 
-export default BookForm
+export default connect()(BookForm);
