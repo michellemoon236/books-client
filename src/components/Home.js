5e58729b0b563c8d0dca0489a4403f022a0b9611
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:14:52 -0700

    add image to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
       <br />
       <h1>Welcome to Book Tally</h1>
       <h3>keep track of the books you want to read</h3>
+      <img src="https://images.unsplash.com/photo-1440778303588-435521a205bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="pile of books" className="img-responsive"/>
     </div>
   )
 }
