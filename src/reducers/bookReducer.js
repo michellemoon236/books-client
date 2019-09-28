880a546e24f4aecb738c143fe2110ac4c446e151
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 19:49:22 -0700

    add action types to bookReducer

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -1,6 +1,19 @@
 export default (state = {books: []}, action ) => {
   switch(action.type) {
-    default: 
+    case 'LOADING_BOOKS':
+    return {
+      // take all the state as it is, except just change this one thing, change loading to true, we use the state spreading which is ...state 
+      // this lets the user know something has happened, we are loading
+      ...state, 
+      loading: true
+    }
+  case 'FETCH_BOOKS':
+    return {
+      ...state,
+      books: action.payload, 
+      loading: false
+    }
+  default: 
     return state
   }
 }
