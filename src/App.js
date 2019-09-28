5677fbfe0bf273bf6ddb306332ce425adb8fb85f
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 19:38:49 -0700

    create genre buttons in genres container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
+import GenresContainer from './containers/GenresContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>Hello World</p>
+        <GenresContainer />
       </div>
     );
   }
--- src/containers/GenresContainer.js
+++ src/containers/GenresContainer.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getGenres } from '../actions/genres';
 
 class GenresContainer extends Component {
+
+  componentDidMount(){
+    this.props.getGenres()
+  }
+
   render() {
+
+    const genres = this.props.genres.map(genre => <button id={genre.id}>{genre.name}</button> )
+
     return (
       <div>
-        
+        { genres }
       </div>
     )
   }
 }
 
-export default connect()(GenresContainer)
+const mapStateToProps = (state) => {
+  // console.log("GENRE CONTAINER I am state", state)
+  return {
+    genres: state.genreReducer.genres
+  }
+}
+
+export default connect(mapStateToProps, {getGenres} )(GenresContainer)
