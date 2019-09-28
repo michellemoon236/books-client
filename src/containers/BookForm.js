afe2f7b26b3a6f28c82e61764605f27ecf7a56c3
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 21:54:50 -0700

    Book Form add default state

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 class BookForm extends Component {
+  state = {
+    title: '',
+    author: '',
+    genre_id: '',
+    image: '',
+    loading: false
+  }
+  
   render() {
     return (
       <div>
