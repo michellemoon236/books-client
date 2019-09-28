82bcec6a94853f1b19ce493a6b48d03296db8e0e
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:56:39 -0700

    add Delete Book action types

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -11,6 +11,19 @@ export default (state = {books: []}, action ) => {
         books: action.payload, 
         loading: false
       }
+    case 'DELETE_BOOK':
+      console.log("Book selected to delete")
+      return {
+        ...state,
+        loading: true
+      }
+    case 'BOOK_DELETED':
+      console.log("Book has been deleted")
+      return {
+        ...state,
+        books: state.books.filter(book => book.id !== action.id),
+        loading: false
+      }
     default: 
       return state
   }
