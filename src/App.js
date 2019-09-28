0ddeafee9139c6865dd5b6ae0af4fba2b1b8f9d3
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 20:31:02 -0700

    add navigation routes to homepage

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
-import GenresContainer from './containers/GenresContainer';
+import Home from './components/Home';
 import BooksContainer from './containers/BooksContainer';
+import BookForm from './containers/BookForm';
 
 class App extends Component {
   render() {
@@ -13,9 +14,12 @@ class App extends Component {
       <div className="App">
         <Router>
           <Navigation />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/books' component={BooksContainer} />
+              <Route exact path='/books/new' component={BookForm} />
+            </Switch>
         </Router>
-        <GenresContainer />
-        <BooksContainer />
       </div>
     );
   }
