87403f1a084a2797218ffa314f64b4fcb04189b4
michellemoon236 <michellemoon236@gmail.com>
2019-09-27 19:23:42 -0700

    create books container

---
+++ src/containers/BooksContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BooksContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BooksContainer 
